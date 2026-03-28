6f3dc97ef2a552c7d9a0410e531f682fcb07a7bc
sushi-shi <sushi.is.shy@gmail.com>
2026-03-28 18:51:43 +0100

    Dev work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const EXECUTABLE: &[u8] = include_bytes!("../resources/survarium.exe");
+const DEBUG_SYMBOLS: &[u8] = include_bytes!("../resources/survarium.pdb");
+
 fn main() {
-    println!("Hello, world!");
+    let pdb = pdb2::PDB::open(std::io::Cursor::new(DEBUG_SYMBOLS)).unwrap();
+    process_executable(pdb);
 }
+
+fn process_executable<S>(pdb: pdb2::PDB<'static, S>) {}
